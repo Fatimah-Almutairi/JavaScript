185c9a82428993e2b2fbe8c8c39a8d3254b5f1ff
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-01-07 23:00:33 +0300

    next

---
+++ components/Navbar.ts
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+function Navbar(){
+    return(
+        <div>
+        <Link href = "/">Home</Link>
+        <Link href = "/about">About</Link>
+        <Link href = "/">Home</Link>
+        </div>
+    )
+}
