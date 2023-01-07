3391280ca439e2ac7bf72ed7cb8c737d6b4e3580
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-01-07 23:05:03 +0300

    next

--- components/Navbar.ts
+++ components/Navbar.ts
@@ -1,8 +1,10 @@
 import Link from "next/link";
 
+import styles from '../styles/Navbar.module.css'
+
 function Navbar(){
     return(
-        <div>
+        <div ClassName={styles.bg}>
         <Link href = "/">Home</Link>
         <Link href = "/about">About</Link>
         <Link href = "/contact">Contact</Link>
