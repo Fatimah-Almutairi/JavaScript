62872bdfe27a09d20e0864dbc52723b75353d01a
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-01-11 22:55:34 +0300

    next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from '../components/Layout'
+// import Layout from '../components/Layout'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
@@ -6,10 +6,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
 
 <>
-  <Layout >
+  {/* <Layout > */}
   return <Component {...pageProps} />
 
-  </Layout>
+  {/* </Layout> */}
   </>
   )
 }
