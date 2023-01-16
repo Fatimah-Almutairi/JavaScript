487fcc03142205917ffb257574172ec810f30fd4
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-01-16 22:14:38 +0300

    react

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import {
   Box,
   theme,
 } from "@chakra-ui/react"
+import Nav from './components/nav'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-
+      <Nav />
     </Box>
   </ChakraProvider>
 )
