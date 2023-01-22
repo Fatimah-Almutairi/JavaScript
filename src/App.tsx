b8a5a682db52d0dbb90c1e84d1000235e9793034
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-01-22 21:56:12 +0300

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
   theme,
 } from "@chakra-ui/react"
 import Nav from './components/nav'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
       <Nav />
-    </Box>
   </ChakraProvider>
 )
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,46 +1,35 @@
-import { ReactNode } from 'react';
 import {
   Box,
   Flex,
-  Link,
-  Button,
   useColorModeValue,
   Stack,
-  useColorMode,
+  Text
 } from '@chakra-ui/react';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}>
-    {children}
-  </Link>
-);
 
-export default function Nav() {
-  const { colorMode, toggleColorMode } = useColorMode();
+function Nav() {
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>Someglora</Box>
-
+        <Flex h={16} alignItems={'center'} justifyContent={'space-around'}>
+          <Box fontSize='1.2em'>BSKIN</Box>
+          <Box>
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-              </Button>
+            <Text color={'#1F5373'}fontSize='1.2em' fontFamily='inherit'>Home Page</Text>
+            <Text color={'#1F5373'}fontSize='1.2em' fontFamily='inherit'>About Us</Text>
+            <Text color={'#1F5373'}fontSize='1.2em' fontFamily='inherit'> Contact</Text>
             </Stack>
           </Flex>
+          </Box>
+
+          <Box>
+
+          </Box>
         </Flex>
       </Box>
     </>
-  );
+  )
 }
+
+export default Nav
