f049479418617b1aa0b6a89d0a1188ee547c4ea3
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-01-24 21:55:31 +0300

    navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -14,6 +14,7 @@ function Nav() {
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-around'}>
+            {/* Logo */}
           <Box fontFamily='fantasy' fontSize='1.4em'>BSKIN</Box>
           <Box>
           <Flex alignItems={'center'}>
