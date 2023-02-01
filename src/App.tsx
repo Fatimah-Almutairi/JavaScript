2047164a2fca57e9d031b8cec973d14ffc9bd6a1
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-02-01 22:36:33 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-// import * as React from "react"
-// import {
-//   ChakraProvider,
-//   theme,
-// } from "@chakra-ui/react"
-// import Nav from './components/nav'
-// // import Carousel from './components/homepage'
+import * as React from "react"
+import {
+  ChakraProvider,
+  theme,
+} from "@chakra-ui/react"
+import Nav from './components/nav'
+// import Carousel from './components/homepage'
 
-// export const App = () => (
-//   <ChakraProvider theme={theme}>
-//       <Nav />
-//       {/* < Carousel /> */}
-//   </ChakraProvider>
-// )
+export const App = () => (
+  <ChakraProvider theme={theme}>
+      <Nav />
+      {/* < Carousel /> */}
+  </ChakraProvider>
+)
