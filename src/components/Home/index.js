e818f8be22b0fc09d8e4e560354f6732bac4eee5
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-02-12 11:29:00 +0300

    react

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,22 @@
-const Home = () => ('Hello')
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+
+const Home = () => {
+
+    return (
+        <div className='container home-page'>
+            <div className='text-zone'>
+                <h1> Hi, <br /> I'm 
+                <img src={LogoTitle} alt='developer' />
+                Fatimah 
+                <br />
+                Web Developer
+                </h1>
+                <h2>Full Stack Developer</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
 
 export default Home
