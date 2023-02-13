136984f164d34067ff4c6b2ede3b954201ac3c82
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-02-13 23:53:38 +0300

    react

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['F', 'a', 't', 'i', 'm', 'a', 'h']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
 
     return (
         <div className='container home-page'>
             <div className='text-zone'>
                 <h1> Hi, <br /> I'm 
                 <img src={LogoTitle} alt='developer' />
-                Fatimah 
+                {/* Fatimah  */}
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                index={15} />
                 <br />
                 Web Developer
                 </h1>
