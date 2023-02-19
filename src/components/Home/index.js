8bd97117087879142cc56f23f850756ef10735b5
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-02-19 22:11:42 +0300

    react

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['F', 'a', 't', 'i', 'm', 'a', 'h']
     const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+    useEffect( () => {
+        return setTimeout( () => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
     return (
         <div className='container home-page'>
