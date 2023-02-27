3d82114857770d6a85a6a1d5974a4ef46637a3df
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2023-02-27 11:08:25 +0300

    react

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -27,6 +27,7 @@ const Home = () => {
     '.',
   ]
 
+  
   useEffect(() => {
     return setTimeout(() => {
       setLetterClass('text-animate-hover')
@@ -35,36 +36,36 @@ const Home = () => {
 
   return (
     <>
-      <div className="container home-page">
-        <div className="text-zone">
-          <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
-            />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-            <br />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={jobArray}
-              idx={22}
-            />
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img
+            src={LogoTitle}
+            alt="JavaScript Developer Name, Web Developer Name"
+          />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
           </h1>
           <h2> Full Stack Developer</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
         </div>
-        {/* < Logo /> */}
+        {/* <Logo /> */}
       </div>
 
       <Loader type="pacman" />
@@ -79,57 +80,78 @@ export default Home
 
 
 
-
-
-
-
-
-
-
-
-
-
-// import { Link } from 'react-router-dom';
-// import LogoTitle from '../../assets/images/logo-s.png';
-// import './index.scss';
-// import { useEffect, useState } from 'react';
-// import AnimatedLetters from '../AnimatedLetters';
+// import { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import Loader from 'react-loaders'
+// import AnimatedLetters from '../AnimatedLetters'
+// import LogoTitle from '../../assets/images/logo-s.png'
+// // import Logo from './Logo'
+// import './index.scss'
 
 // const Home = () => {
-//     const [letterClass, setLetterClass] = useState('text-animate');
-//     const nameArray = ['F', 'a', 't', 'i', 'm', 'a', 'h']
-//     const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+//   const [letterClass, setLetterClass] = useState('text-animate')
 
-//     useEffect(() => {
-//         return setTimeout(() => {
-//           setLetterClass('text-animate-hover')
-//         }, 4000)
-//       }, [])
+//   const nameArray = ['F', 'a', 't', 'i', 'm', 'a', 'h']
+//   const jobArray = [
+//     'w',
+//     'e',
+//     'b',
+//     ' ',
+//     'd',
+//     'e',
+//     'v',
+//     'e',
+//     'l',
+//     'o',
+//     'p',
+//     'e',
+//     'r',
+//     '.',
+//   ]
 
-//     return (
-//         <div className='container home-page'>
-//             <div className='text-zone'>
-//                 <h1><span className={letterClass}>H</span>
-//                 <span className={ `${letterClass} _12`}>i</span>
-//                 <br />
-//                 <span className={ `${letterClass} _12`}>I</span>
-//                 <span className={ `${letterClass} _12`}>'m</span>
-//                 <img src={LogoTitle} alt='developer' />
-//                 {/* Fatimah  */}
-//                 <AnimatedLetters letterClass={letterClass}
-//                 strArray={nameArray}
-//                 index={15} />
-//                 <br />
-//                 {/* Web Developer */}
-//                 <AnimatedLetters letterClass={letterClass}
-//                 strArray={jobArray}
-//                 index={22} />
-//                 </h1>
-//                 <h2>Full Stack Developer</h2>
-//                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-//             </div>
+//   useEffect(() => {
+//     return setTimeout(() => {
+//       setLetterClass('text-animate-hover')
+//     }, 4000)
+//   }, [])
+
+//   return (
+//     <>
+//       <div className="container home-page">
+//         <div className="text-zone">
+//           <h1>
+//             <span className={letterClass}>H</span>
+//             <span className={`${letterClass} _12`}>i,</span>
+//             <br />
+//             <span className={`${letterClass} _13`}>I</span>
+//             <span className={`${letterClass} _14`}>'m</span>
+//             <img
+//               src={LogoTitle}
+//               alt="JavaScript Developer Name, Web Developer Name"
+//             />
+//             <AnimatedLetters
+//               letterClass={letterClass}
+//               strArray={nameArray}
+//               idx={15}
+//             />
+//             <br />
+//             <AnimatedLetters
+//               letterClass={letterClass}
+//               strArray={jobArray}
+//               idx={22}
+//             />
+//           </h1>
+//           <h2> Full Stack Developer</h2>
+//           <Link to="/contact" className="flat-button">
+//             CONTACT ME
+//           </Link>
 //         </div>
-//     )
+//         {/* < Logo /> */}
+//       </div>
+
+//       <Loader type="pacman" />
+//     </>
+//   )
 // }
 
 // export default Home
